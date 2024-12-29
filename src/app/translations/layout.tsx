f2f8f55cc91595580a2d8fbd9d0840f9dad1f909
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-12-29 19:50:40 +0530

    Layout Updated for translations Page

--- src/app/translations/layout.tsx
+++ src/app/translations/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <main className="min-h-screen bg-gray-100 py-8 max-w-screen-xl">
+        <main className="min-h-screen bg-gray-100 py-8 w-screen-xl">
             <div className="container mx-auto px-4">{children}</div>
         </main>
     );
